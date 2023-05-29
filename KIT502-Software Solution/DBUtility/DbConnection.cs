46e457561cdac4a62d64757569f88c3d11646a3f
razi061 <129680513+razi061@users.noreply.github.com>
2023-05-29 16:53:21 +1000

    ProductDetails & ProductEdit Forms

---
+++ KIT502-Software Solution/DBUtility/DbConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+namespace KIT502_Software_Solution.DBUtility
+{
+    internal class DbConnection
+    {
+        private static MySqlConnection? conn;
+
+        private const string db = "group24";
+        private const string user = "group24";
+        private const string pass = "gpass";
+        private const string server = "131.217.174.230";
+
+        private static MySqlConnection GetConnection()
+        {
+            if (conn == null || (conn.State == System.Data.ConnectionState.Broken || 
+                conn.State == System.Data.ConnectionState.Closed))
+            {
+                string connectionString = String.Format("Database={0};Data Source={1};User Id={2};Password={3}", db, server, user, pass);
+                conn = new MySqlConnection(connectionString);
+            }
+
+            return conn;
+        }
+    }
+}
---
+++ KIT502-Software Solution/Model/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT502_Software_Solution.Model
+{
+    class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string low_discount_level { get; set; }
+        public string high_discount_level { get; set; }
+
+        public Category() 
+        { 
+            this.id = 0;
+            this.name = string.Empty;
+            this.low_discount_level = string.Empty;
+            this.high_discount_level = string.Empty;
+        }
+    }
+}
